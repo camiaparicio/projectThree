562bba7429b47a1b433e3e418aef08cc9ac0f0ed
Camila <camilaaparicioo@hotmail.com>
2021-11-20 12:25:12 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+// on page load, make a call to the store api
+// store the data from the api in state
+// display the results to the page
+//  -> img and title of object
+
+// site: https://fakestoreapi.com/docs & no api key
+
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // piece of state to hold the products
+  const [product, setProduct] = useState([])
+
+  useEffect( () => {
+  // in here well call the api using axios
+
+  axios({
+    url: 'https://fakestoreapi.com/products/categories', 
+    method: 'GET',
+    responseType: 'json',
+    // params: {
+    //   
+    // }
+  })
+  .then(response => {
+    console.log(response)
+    setProduct(response.data)
+  })
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <header>
+          <h1>welcome to the fake store </h1>
+
+          
+              <div className="sideNav">
+                <ul>
+                {
+                  product.map( data => {
+                    return(
+                      
+                    <li> <a href="#">{data}</a> </li>
+                    )
+                  })
+                }
+                </ul>
+              </div>
+
+
+        </header>
+      </div>
     </div>
   );
 }
